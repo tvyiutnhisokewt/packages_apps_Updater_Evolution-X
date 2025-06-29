b06dbbbc0123a3dc038be8f4c87b9aec110bee75
nhansp <nhan.094.vn@gmail.com>
2025-06-30 06:13:44 +0700

    Revert "Updater: Set User-Agent to EvoXUpdater/1.0"
    This reverts commit 23a0bcfeb48cebc717a6838ad29a7f38b8bbe754.

--- app/src/main/java/org/evolution/updater/download/HttpURLConnectionClient.java
+++ app/src/main/java/org/evolution/updater/download/HttpURLConnectionClient.java
@@ -58,7 +58,6 @@ public class HttpURLConnectionClient implements DownloadClient {
             DownloadClient.DownloadCallback callback,
             boolean useDuplicateLinks) throws IOException {
         mClient = (HttpURLConnection) new URL(url).openConnection();
-        mClient.setRequestProperty("User-Agent", "EvoXUpdater/1.0");
         mDestination = destination;
         mProgressListener = progressListener;
         mCallback = callback;
@@ -182,7 +181,6 @@ public class HttpURLConnectionClient implements DownloadClient {
             if (range != null) {
                 mClient.setRequestProperty("Range", range);
             }
-            mClient.setRequestProperty("User-Agent", "EvoXUpdater/1.0");
         }
 
         private void handleDuplicateLinks() throws IOException {
